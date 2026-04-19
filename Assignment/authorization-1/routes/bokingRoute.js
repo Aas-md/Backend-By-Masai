bc0c0f8b7561ce993d0f3e1173e91ee2a5e717a2
Aas MOhammad <aasmohd7071@gmail.com>
2026-04-19 14:27:41 +0530

    done

--- Assignment/authorization-1/routes/bokingRoute.js
+++ Assignment/authorization-1/routes/bokingRoute.js
@@ -14,6 +14,10 @@ router.post('/', isLoggedIn, async (req, res) => {
 
         let user = await userModel.findById(req.user.userId)
 
+        if(user.role === 'admin'){
+            res.status(403).json({ msg: "Admins cannot create bookings" })
+        }
+
         let booking = await bookingModel.create({ ...req.body, createdBy: user._id })
 
         res.json({ msg: "Booking created successfully", booking })
@@ -22,6 +26,21 @@ router.post('/', isLoggedIn, async (req, res) => {
         res.status(500).json({ msg: "Something went wrong", error: err.message })
     }
 
+})
+
+router.get('/', isLoggedIn, async (req, res) => {
+    try {
+        let user = req.user
+        let currentUser = await userModel.findById(user.userId)
+        if(currentUser.role === 'admin') {
+           let allBookings = await bookingModel.find().populate('createdBy', 'name email')
+           return res.json({ msg: "All bookings fetched successfully", bookings: allBookings })
+        }
+        let bookings = await bookingModel.find({ createdBy: user.userId }).populate('createdBy', 'name email')
+        res.json({ msg: "Bookings fetched successfully", bookings })
+    } catch (err) {
+        res.status(500).json({ msg: "Something went wrong", error: err.message })
+    }
 })
 
 module.exports = router
