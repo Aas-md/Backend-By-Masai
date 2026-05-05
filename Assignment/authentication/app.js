1d86b55dd7b3e419b88840fdc0b820853b65c9ed
Aas MOhammad <aasmohd7071@gmail.com>
2026-05-05 18:42:12 +0530

    working on authentication

---
+++ Assignment/authentication/app.js
@@ -0,0 +1,24 @@
+let express = require('express')
+let app = express()
+let UserModel = require('./userModel')
+const mongoose = require('mongoose');
+
+
+async function connectToDb() {
+    try {
+       await  mongoose.connect('mongodb://127.0.0.1:27017/authentication')
+        console.log('Connected!')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+connectToDb()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+})
